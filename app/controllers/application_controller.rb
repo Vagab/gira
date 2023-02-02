a365fdb912df7b0db892589d9d2f804f37a7af0c
barseek <sergey.b@hey.com>
2023-02-02 21:50:37 +0100

    Add some profiles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
+
+  def signed_in_root_path(_resource)
+    user_path
+  end
 end
